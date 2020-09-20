01c9c392e657c8451410a4a28f5c35e26e455ec5
Malwina Czepiżak <malwina.czepizak@gmail.com>
2020-09-20 15:12:41 +0200

    prepared folders and files

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  //tu może być Routing
+  return (
+    <div className="App">
+    <h1>Siema Elo</h1>
+    </div>
+  );
+}
+
+export default App;
